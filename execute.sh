f2ed094a6bd2d11a70c2d9017c100d89b78d01d3
Aditya Pingale <aditya22pingale@gmail.com>
2024-10-24 20:38:41 +0530

    add gitignore

--- execute.sh
+++ execute.sh
@@ -1,12 +1,13 @@
-@echo off
-REM Step 1: Create a Python virtual environment named "myenv"
+#!/bin/bash
+
+# Step 1: Create a Python virtual environment named "myenv"
 python -m venv myenv
 
-REM Step 2: Activate the virtual environment
-call myenv\Scripts\activate
+# Step 2: Activate the virtual environment
+source myenv/Scripts/activate
 
-REM Step 3: Install required Python packages from 'requirements.txt'
+# Step 3: Install required Python packages from 'requirements.txt'
 pip install -r requirements.txt
 
-REM Step 4: Run the Python file 'web_app.py'
+# Step 4: Run the Python file 'web_app.py'
 python web_app.py
